3d935ca7b8697b035bd785d3e32d697968ddb93c
Mervyn Galvin <mervyn511@gmail.com>
2019-11-14 12:35:51 +0000

    fix getting system version, update .env.example
    System version using os.system only returned exit code, oops! Using subprocess.checkoutput now.
    Removed unused import

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 import os
+import subprocess
 import requests
-
-import settings
 
 from packaging import version
 
@@ -12,16 +11,22 @@ build = os.getenv("BUILD")
 plex = os.getenv("PLEX_LOCATION")
 download_location = os.getenv("DOWNLOAD_LOCATION")
 
-
 result = requests.get('https://plex.tv/pms/downloads/5.json')
 result = result.json()
 
 result = result['computer'][system_type]
 current_version = version.parse(result['version'])
-system_version = str(os.system(plex + " -- version"))
+try:
+    system_version = subprocess.check_output(
+        [plex, '--version']).decode("utf-8").strip()
+except subprocess.CalledProcessError as e:
+    print("""An error occured when getting the system version of Plex.
+Check that you have the correct location in your .env and try again.""")
+    print(repr(e))
+    exit(1)
+
 system_version = version.parse(system_version) if not system_version.startswith(
     "v") else version.parse(system_version[1:])
-
 if current_version > system_version:
     print("Update Found!")
 else:
